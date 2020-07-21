137bfba3c50ec67489d8270eb72fedaf8d3e99c6
Josejulio Martínez <jmartine@redhat.com>
2020-07-21 15:22:42 -0500

    Allow karma support (#9)
    - Using jestMock.fn instead of jest.fn
     - Reading `insights` always from `window`

--- packages/insights-common-typescript/src/utils/Insights.ts
+++ packages/insights-common-typescript/src/utils/Insights.ts
@@ -3,6 +3,7 @@
 // Is possible that there is something wrong and/or missing, but as I was using this on more than one file it seems like
 // a good idea to have all the usage in a single file and define a common interface to keep track of it.
 // It would be even better to add the typings to the common code or to @types.
+import jestMock from 'jest-mock';
 
 interface Entitlement {
     is_entitled: boolean;
@@ -49,38 +50,42 @@ export type InsightsType = {
     };
 };
 
-declare const insights: InsightsType;
+interface Window {
+    insights: InsightsType;
+}
+
+declare const window: Window;
 
 let insightPromise: Promise<InsightsType>;
 
 export const waitForInsights = (): Promise<InsightsType> => {
     if (!insightPromise) {
         insightPromise = new Promise<InsightsType>(async (resolve) => {
-            while (!global.hasOwnProperty('insights')) {
+            while (!window.hasOwnProperty('insights')) {
                 await new Promise(timeout => setTimeout(timeout, 250));
             }
 
-            resolve(insights);
+            resolve(window.insights);
         });
     }
 
     return insightPromise;
 };
 
-export const getInsights = (): InsightsType => insights;
+export const getInsights = (): InsightsType => window.insights;
 
 export const mockInsights = (mock?: InsightsType) => {
-    (global as any).insights = mock || {
+    window.insights = mock || {
         chrome: {
-            init: jest.fn(),
-            identifyApp: jest.fn((_appId: string) => Promise.resolve()),
-            on: jest.fn((type: string, callback: ((event: any) => void)) => {
+            init: jestMock.fn(),
+            identifyApp: jestMock.fn((_appId: string) => Promise.resolve()),
+            on: jestMock.fn((type: string, callback: ((event: any) => void)) => {
                 callback(new Event('fake'));
             }),
             isProd: false,
-            isBeta: jest.fn(() => true),
+            isBeta: jestMock.fn(() => true),
             auth: {
-                getUser: jest.fn(() => Promise.resolve({
+                getUser: jestMock.fn(() => Promise.resolve({
                     identity: {
                         account_number: '123456',
                         internal: {
